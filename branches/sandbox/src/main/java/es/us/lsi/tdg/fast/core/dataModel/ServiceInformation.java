249cc52dbfb4992684c6a78d33639b47b67cff79
pafmon <pafmon@1dd39048-c632-0410-b676-f7962134b12f>
2007-07-21 11:45:43 +0000

    CP Knowledge

---
+++ branches/sandbox/src/main/java/es/us/lsi/tdg/fast/core/dataModel/ServiceInformation.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+package es.us.lsi.tdg.fast.core.dataModel;
+
+public interface ServiceInformation {
+
+}
---
+++ branches/sandbox/src/main/java/es/us/lsi/tdg/fast/core/dataModel/TradingInformation.java
@@ -0,0 +1,5 @@
+package es.us.lsi.tdg.fast.core.dataModel;
+
+public interface TradingInformation {
+
+}
