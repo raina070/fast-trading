8cb531af43e049d41ee8cc973d7d8fdf715291f0
pafmon <pafmon@1dd39048-c632-0410-b676-f7962134b12f>
2007-07-30 18:11:05 +0000

    Domain Registry and TradingOrchestrator interfaces

---
+++ src/main/java/es/us/lsi/tdg/fast/core/domainRegistry/BaseDomainRegistry.java
@@ -0,0 +1,37 @@
+package es.us.lsi.tdg.fast.core.domainRegistry;
+
+import java.util.Set;
+
+import es.us.lsi.tdg.fast.core.dataModel.statement.AttributeCatalog;
+
+public class BaseDomainRegistry implements DomainRegistry {
+
+	
+	
+	
+	public Set<String> getAllDomains(String domainName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public AttributeCatalog getAttributeCatalog(String domainName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void loadDomain(String domainName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean searchDomain(String domainName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void unloadDomain(String domainName) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
