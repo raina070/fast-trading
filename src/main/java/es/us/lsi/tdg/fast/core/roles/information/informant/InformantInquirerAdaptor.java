e537f44b39f0fc7213c48c263e57cdd004a5cafe
japarejo <japarejo@1dd39048-c632-0410-b676-f7962134b12f>
2007-08-09 20:16:58 +0000

    Forgotten form previous commit.

---
+++ src/main/java/es/us/lsi/tdg/fast/core/roles/information/informant/InformantInquirerAdaptor.java
@@ -0,0 +1,8 @@
+package es.us.lsi.tdg.fast.core.roles.information.informant;
+
+import es.us.lsi.tdg.fast.core.roles.information.Informant;
+import es.us.lsi.tdg.fast.core.roles.information.Inquirer;
+
+public interface InformantInquirerAdaptor extends Inquirer{
+
+}
