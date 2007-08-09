3ec59e2e27dec014fd66a53e3ac0dfa55973a2f8
japarejo <japarejo@1dd39048-c632-0410-b676-f7962134b12f>
2007-08-09 20:15:23 +0000

    Forgotten form previous commit.

---
+++ src/main/java/es/us/lsi/tdg/fast/domains/fom/components/FOMInformation/process/FOMInformantProcess.java
@@ -0,0 +1,18 @@
+package es.us.lsi.tdg.fast.domains.fom.components.FOMInformation.process;
+
+import es.us.lsi.tdg.fast.core.roles.AbstractControllableProcess;
+import es.us.lsi.tdg.fast.core.roles.information.Informant;
+import es.us.lsi.tdg.fast.domains.fom.components.FOMInformation.FOMInformation;
+
+public class FOMInformantProcess extends AbstractControllableProcess {
+
+	private FOMInformation informationComponent;
+	
+	protected Informant informant;
+	
+	@Override
+	protected void run() {
+			
+	}
+
+}
